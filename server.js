d2dd55af596f75c24809a42036201ff1f62f2b4b
Matt Kim <matthkim@gmail.com>
2018-01-19 01:14:30 -0800

    Fix paths

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ module.exports = {
   app: function () {
     const app = express()
     const indexPath = path.join(__dirname, 'build_webpack', 'index.html')
+    const assetManifest = path.join(__dirname, 'build_webpack', 'asset-manifest.json')
+
     const staticPath = express.static(path.join(__dirname, 'build_webpack', 'static'))
     const fontsPath = express.static(path.join(__dirname, 'build_webpack', 'fonts'))
-    const assetManifest = express.static(path.join(__dirname, 'build_webpack', 'asset-manifest.json'))
-
+    
     app.use('/static', staticPath)
     app.use('/fonts', fontsPath)
     app.get('/', function (_, res) { res.sendFile(indexPath) })
